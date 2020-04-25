931ba7e90f499aea4b2e80f274bf2384bb21ed40
Artem Vityuk <avityuk@mail.greenriver.edu>
2020-04-25 00:59:46 -0400

    index.php step 1

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+Artem Vityuk
+4/17/2020
+https://github.com/ArtemVit1996/pp2.git
+ **/
+
+// error reporting for debugging only
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+    <header>
+        <h1>Pair Program 2</h1>
+    </header>
+</head>
+<body>
+<?php
+echo "<h2>----- Step 1 -----</h2>";
+echo "<h3>PHP Array Practice</h3><br>";
+?>
+
+</body>
+</html>
